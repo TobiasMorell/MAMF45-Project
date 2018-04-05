69b8f641ce10f78ab6b8e0b869ac618f45d6677d
Mattias-G <dat13mgu@student.lu.se>
2018-04-05 11:51:50 +0200

    Added a test scene with an orbiting camera

---
+++ MAMF45/Assets/Scripts/OrbitCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCam : MonoBehaviour {
+
+	[SerializeField]
+	private bool isPerspective = true;
+	[SerializeField]
+	private Vector3 target;
+	//[SerializeField]
+	//private GameObject balloon;
+	private Camera cam;
+	private Vector3 distance;
+	
+	void Start () {
+		cam = GetComponent<Camera>();
+		distance = new Vector3(0f, 0f, (transform.position - target).magnitude);
+	}
+	
+	void Update ()
+	{
+		transform.Rotate(0f, -Input.GetAxis("Horizontal"), 0f, Space.World);
+		transform.Rotate(Input.GetAxis("Vertical"), 0f, 0f, Space.Self);
+		transform.position = target - transform.rotation * distance;
+
+		if (Input.GetKeyDown("p"))
+		{
+			isPerspective = !isPerspective;
+			cam.orthographic = !isPerspective;
+		}
+
+		//if (Input.GetKeyDown("b"))
+		//{
+		//	var clone = Instantiate(balloon, transform.position, transform.rotation);
+		//	clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * 10f);
+		//}
+
+		distance.z -= Input.GetAxis("Mouse ScrollWheel") * 5f;
+	}
+}
