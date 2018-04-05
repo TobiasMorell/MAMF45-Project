ac378be3f70e6776095c6b4d34b750ea5e54f273
Sebastian-0 <melon.bag@hotmail.com>
2018-04-05 11:42:34 +0200

    Added a health script for animals.

---
+++ MAMF45/Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    private bool isSick;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+		// TODO Code here to infect others periodically? 
+	}
+
+
+    public void Infect()
+    {
+        isSick = true;
+    }
+
+    public bool IsSick()
+    {
+        return isSick;
+    }
+
+    public bool IsHealthy()
+    {
+        return !isSick;
+    }
+}
