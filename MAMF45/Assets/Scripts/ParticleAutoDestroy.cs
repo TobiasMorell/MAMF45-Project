7e8326e410c8181643e8975f5e9cb02e1602432b
Sebastian Hjelm <dat13shj@gmail.com>
2018-04-13 09:03:09 +0200

    Added an auto destroy script for particle effects.

---
+++ MAMF45/Assets/Scripts/ParticleAutoDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAutoDestroy : MonoBehaviour {
+
+	void Start ()
+	{
+		var particle = GetComponent<ParticleSystem> ();
+		Destroy (gameObject, particle.main.duration);
+	}
+}
