2a543c02df77230a637385600ed1222f2cc0fa38
Sebastian Hjelm <dat13shj@student.lu.se>
2018-04-13 11:11:36 +0200

    Added a napkin to prevent sneezing.

--- MAMF45/Assets/Scripts/Health.cs
+++ MAMF45/Assets/Scripts/Health.cs
@@ -2,37 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour
+{
+	private int MAXIMUM_SNEEZE_INTERVAL = 2;
+	private float SNEEZE_RADIUS = 0.2f;
 
-    private int MAXIMUM_SNEEZE_INTERVAL = 20;
-    private float SNEEZE_RADIUS = 0.2f;
-
-    public bool StartInfected = false;
+	public bool StartInfected = false;
 
 	public GameObject ParticleEffect;
 
-    private bool isSick;
-    private float sneezeTimer;
+	private bool isSick;
+	private float sneezeTimer;
 
 	void Start ()
-    {
+	{
 		if (StartInfected) {
 			Infect ();
 		}
 	}
-	
+
 	void Update ()
-    {
+	{
 		if (isSick) {
 			sneezeTimer -= Time.deltaTime;
 			if (sneezeTimer < 0) {
-				Instantiate (ParticleEffect, transform.position + transform.forward * 0.374f * 0.2f, transform.rotation);
+				var nose = GetComponentInChildren<Nose> ();
+				if (!nose.IsCovered ()) {
+					var nosePosition = GetNosePosition();
+					Instantiate (ParticleEffect, nosePosition, transform.rotation);
 
-				var colliders = Physics.OverlapSphere (transform.position + transform.forward * SNEEZE_RADIUS, SNEEZE_RADIUS);
-				foreach (var collider in colliders) {
-					var health = collider.GetComponent<Health> ();
-					if (collider.gameObject != gameObject && health != null) {
-						health.Infect ();
+					var colliders = Physics.OverlapSphere (nosePosition + transform.forward * SNEEZE_RADIUS, SNEEZE_RADIUS);
+					foreach (var collider in colliders) {
+						var health = collider.GetComponent<Health> ();
+						if (collider.gameObject != gameObject && health != null) {
+							health.Infect ();
+						}
 					}
 				}
 				ResetSneezeTimer ();
@@ -40,44 +44,47 @@ public class Health : MonoBehaviour {
 		}
 	}
 
+	private Vector3 GetNosePosition() {
+		var nose = GetComponentInChildren<Nose> ();
+		return nose.transform.position;
+	}
 
-    public void Infect()
-    {
-        if (!isSick)
-		{
+
+	public void Infect ()
+	{
+		if (!isSick) {
 			ResetSneezeTimer ();
-            print("New rabbit infected!");
-        }
-        isSick = true;
-    }
+			print ("New rabbit infected!");
+		}
+		isSick = true;
+	}
 
-	private void ResetSneezeTimer()
+	private void ResetSneezeTimer ()
 	{
 		sneezeTimer = Random.Range (0f, MAXIMUM_SNEEZE_INTERVAL);
 	}
 
-	public void Cure() 
+	public void Cure ()
 	{
 		isSick = false;
 	}
-		
 
-    public bool IsSick()
-    {
-        return isSick;
-    }
 
-    public bool IsHealthy()
-    {
-        return !isSick;
-    }
+	public bool IsSick ()
+	{
+		return isSick;
+	}
 
-    void OnDrawGizmos()
-    {
-        if (isSick)
-        {
-            Gizmos.color = new Color(1, 0, 0, 0.5f);
-            Gizmos.DrawWireSphere(transform.position + transform.forward * SNEEZE_RADIUS, SNEEZE_RADIUS);
-        }
-    }
+	public bool IsHealthy ()
+	{
+		return !isSick;
+	}
+
+	void OnDrawGizmos ()
+	{
+		if (isSick) {
+			Gizmos.color = new Color (1, 0, 0, 0.5f);
+			Gizmos.DrawWireSphere (GetNosePosition() + transform.forward * SNEEZE_RADIUS, SNEEZE_RADIUS);
+		}
+	}
 }
---
+++ MAMF45/Assets/Scripts/Nose.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nose : MonoBehaviour {
+
+	private bool isCovered;
+
+	public bool IsCovered() {
+		return isCovered;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.GetComponent<Napkin> ()) {
+			isCovered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.GetComponent<Napkin> ()) {
+			isCovered = false;
+		}
+	}
+}
