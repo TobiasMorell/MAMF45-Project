9c0116846e02f20d00e5c85ca9d70d2ce6d475a5
Tobias Morell <tmorel14@student.aau.dk>
2018-04-18 14:25:50 +0200

    Attempted to fix collision issues between buttons and toolstand

---
+++ MAMF45/Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour {
+	public List<Collider> IgnoredColliders;
+
+	// Use this for initialization
+	void Start () {
+		var ownCollider = GetComponent<Collider> ();
+		foreach (var col in IgnoredColliders) {
+			Physics.IgnoreCollision (ownCollider, col);
+		}
+	}
+}
