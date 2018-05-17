eba74625e2c6e2aefeaba60200f0ae1754591d2a
Mattias-G <dat13mgu@student.lu.se>
2018-05-17 13:54:36 +0200

    Modified the scene slightly

---
+++ MAMF45/Assets/Scripts/IntroSlideShow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSlideShow : MonoBehaviour {
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = Camera.main.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(0,player.rotation.eulerAngles.y,0);
+	}
+}
