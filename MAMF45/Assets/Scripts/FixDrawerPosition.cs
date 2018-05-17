b9df3e24304f9d0a4ddd0a9324065712a153aead
Mattias-G <dat13mgu@student.lu.se>
2018-05-17 10:29:44 +0200

    Limited drawer movement

---
+++ MAMF45/Assets/Scripts/FixDrawerPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixDrawerPosition : MonoBehaviour {
+
+    private float maxX;
+    private float minX;
+
+	void Start ()
+    {
+        maxX = transform.localPosition.x + 0.118f;
+        minX = transform.localPosition.x - 0.6f;
+        Debug.Log(maxX + " < > " + minX);
+    }
+	
+	void Update () {
+        if (transform.localPosition.x < minX)
+        {
+            transform.localPosition = new Vector3(minX, transform.localPosition.y, transform.localPosition.z);
+            Debug.Log("MIN HIT!");
+        }
+        if (transform.localPosition.x > maxX)
+        {
+            Debug.Log("MAX HIT!");
+            transform.localPosition = new Vector3(maxX, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+}
