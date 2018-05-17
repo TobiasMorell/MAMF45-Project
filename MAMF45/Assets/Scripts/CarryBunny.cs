e1d96e86f140974d5c1f4bd90bd88aba054a0fb6
Sebastian Hjelm <dat13shj@student.lu.se>
2018-05-17 10:10:21 +0200

    Made bunny carrying more stable.

--- MAMF45/Assets/Scripts/CarryBunny.cs
+++ MAMF45/Assets/Scripts/CarryBunny.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 public class CarryBunny : MonoBehaviour {
+    private Vector3 carryOffset;
+    private bool isGrabbed;
 
-	// Use this for initialization
-	void Awake () {
+    void Awake () {
 		var interacteble = GetComponent<Interactable>();
 		interacteble.onAttachedToHand += OnAttachedToHandDelegate;
 		interacteble.onDetachedFromHand += OnDetachedFromHandDelegate;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    void Update() {
+        var euler = transform.rotation.eulerAngles;
+        if (isGrabbed) {
+            transform.position = transform.parent.transform.position + carryOffset;
+        }
+        transform.rotation = Quaternion.Euler(0, euler.y, 0);
+    }
 
-
-	public void OnAttachedToHandDelegate(Hand hand) {
-		GetComponent<Animator>().SetTrigger("PickedUp");
-		var grabPoint = transform.Find ("GrabPoint");
-		transform.position = hand.transform.position - grabPoint.position;
-	}
+    public void OnAttachedToHandDelegate(Hand hand) {
+        carryOffset = transform.position - transform.parent.position;
+        isGrabbed = true;
+    }
 
 	public void OnDetachedFromHandDelegate(Hand hand) {
-		GetComponent<Animator>().SetTrigger("Dropped");
-	}
+        isGrabbed = false;
+    }
 }
