f48b1c4fc7bffedf4ddb21b472deaae4afd34849
Mattias-G <dat13mgu@student.lu.se>
2018-05-17 10:41:26 +0200

    Moved an object in the scene

--- MAMF45/Assets/Scripts/FixDrawerPosition.cs
+++ MAMF45/Assets/Scripts/FixDrawerPosition.cs
@@ -11,18 +11,15 @@ public class FixDrawerPosition : MonoBehaviour {
     {
         maxX = transform.localPosition.x + 0.118f;
         minX = transform.localPosition.x - 0.6f;
-        Debug.Log(maxX + " < > " + minX);
     }
 	
 	void Update () {
         if (transform.localPosition.x < minX)
         {
             transform.localPosition = new Vector3(minX, transform.localPosition.y, transform.localPosition.z);
-            Debug.Log("MIN HIT!");
         }
         if (transform.localPosition.x > maxX)
         {
-            Debug.Log("MAX HIT!");
             transform.localPosition = new Vector3(maxX, transform.localPosition.y, transform.localPosition.z);
         }
     }
