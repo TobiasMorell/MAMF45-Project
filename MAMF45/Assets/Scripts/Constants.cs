ecf7c4cf83890c631812fed80c8a6ff850f051b9
Mattias-G <dat13mgu@student.lu.se>
2018-05-21 14:10:30 +0200

    Rebalanced the game

--- MAMF45/Assets/Scripts/Constants.cs
+++ MAMF45/Assets/Scripts/Constants.cs
@@ -12,10 +12,30 @@ public class Constants : MonoBehaviour {
 		if (Instance != null)
 			Debug.LogError ("More than one 'Timers' exists in the scene. Please make sure only to place one at a time!");
 		Instance = this;
+
+        if (DifficultyLevel > 1)
+        {
+            MaxBunnyCount += DifficultyLevel;
+            SpawnRate -= DifficultyLevel;
+            SwarmRate -= DifficultyLevel * 10;
+            TimerColdSneezeMin -= DifficultyLevel;
+            TimerColdSneezeMax -= DifficultyLevel;
+            TimerPneumoniaSneezeMin -= DifficultyLevel;
+            TimerPneumoniaSneezeMax -= DifficultyLevel;
+            TimerPneumoniaDeathMin -= DifficultyLevel;
+            TimerLoveIntervals -= DifficultyLevel*2;
+            TimerLoveReactionTime -= DifficultyLevel;
+        }
 	}
 
     [Header("General")]
     public bool HasGameBegun = false;
+    public int DifficultyLevel = 1;
+
+    [Header("Bunny spawn")]
+    public int MaxBunnyCount = 8;
+    public int SpawnRate = 10;
+    public int SwarmRate = 120;
 
     [Header("Cold Timers")]
 	public float TimerColdSneezeMax = 20f;
@@ -24,24 +44,24 @@ public class Constants : MonoBehaviour {
 	public float TimerColdCureMin = 25f;
 
 	[Header("Pneumonia Timers")]
-	public float TimerPneumoniaSneezeMax = 60f;
-	public float TimerPneumoniaSneezeMin = 40f;
-	public float TimerPneumoniaDeathMax = 100f;
-	public float TimerPneumoniaDeathMin = 80f;
+	public float TimerPneumoniaSneezeMax = 30f;
+	public float TimerPneumoniaSneezeMin = 15;
+	public float TimerPneumoniaDeathMax = 60f;
+	public float TimerPneumoniaDeathMin = 50f;
 
 	[Header("Love Timers")]
-	public float TimerLoveIntervals = 30f;
-	public float TimerLoveReactionTime = 15f;
+	public float TimerLoveIntervals = 20f;
+	public float TimerLoveReactionTime = 5f;
 
 	[Header("Misc Timers")]
-	public float TimerTriggerHealthyBunny = 60f;
+	public float TimerTriggerHealthyBunny = 20f;
 	public float BunnyDespawnDelay = 75f;
 	public float GameTime = 150f;
 
 	[Header("Scores")]
 	public int ScoreBunnyDied = -50;
-	public int ScoreBunnyHeartSaved = 10;
+	public int ScoreBunnyHeartSaved = 30;
 	public int ScoreBonusHole = 20;
-	public int ScoreRecycle = 1;
-	public int ScoreBunnyNoHeartSaved = 1;
+	public int ScoreRecycle = 5;
+	public int ScoreBunnyNoHeartSaved = 10;
 }
--- MAMF45/Assets/Scripts/Tags.cs
+++ MAMF45/Assets/Scripts/Tags.cs
@@ -2,8 +2,9 @@
 
 public static class Tags
 {
-	public const string BUNNY = "Bunny";
-	public const string BUTTON = "Button";
+    public const string DEFAULT = "Untagged";
+    public const string BUNNY = "Bunny";
+    public const string BUTTON = "Button";
 	public const string CONTRACEPTIVE = "ContraceptiveTrigger";
 	public const string FINISH = "Finish";
 	public const string FENCE = "Fence";
